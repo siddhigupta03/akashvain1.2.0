ad7d02cb733575c6c8a91a42903071fb39b33880
siddhigupta03 <76785581+siddhigupta03@users.noreply.github.com>
2022-01-03 17:44:08 +0530

    second commit

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, TouchableOpacity, Platform, SafeAreaView, Image, Text } from "react-native";
+import { StyleSheet, View, Image, Text } from "react-native";
 import * as Font from "expo-font"; 
-import AppLoading from 'expo-app-loading';
+import { RFValue } from "react-native-responsive-fontsize";
 
 let costoms = {
     'Bubblegum': require("../assets/fonts/BubblegumSans-Regular.ttf")
@@ -51,19 +51,19 @@ export default class Home extends Component {
                     
                     <View style={styles.weatherCon}>
                         <View>
-                            <Text>Description: {this.state.weather[0].descriptions}</Text>
+                            <Text style={styles.weatherText}>Description: {this.state.weather[0].descriptions}</Text>
                         </View>
 
                         <View>
-                            <Text>Humidity: {this.state.weather.humidity}</Text>
+                            <Text style={styles.weatherText}>Humidity: {this.state.weather.humidity}</Text>
                         </View>
 
                         <View>
-                            <Text>Minimum temperature: {this.state.weather.main.temp_min}</Text>
+                            <Text style={styles.weatherText}>Minimum temperature: {this.state.weather.main.temp_min}</Text>
                         </View>
 
                         <View>
-                            <Text>Maximum temperature: {this.state.weather.main.temp_max}</Text>
+                            <Text style={styles.weatherText}>Maximum temperature: {this.state.weather.main.temp_max}</Text>
                         </View>
                     </View>
                     
@@ -72,10 +72,45 @@ export default class Home extends Component {
                     </View>
 
                     <View style={styles.whatToTake}>
-                       {this.state.weather.main.temp_min === "<29" ? <Text>Please don't forget to take your napkin.</Text> : null }
+                       {this.state.weather.main.temp_min < 29  ? <Text>Please don't forget to take your napkin.</Text> : null }
                     </View>
                 </View>
             );
         }
     }
 }
+
+const styles = StyleSheet.create({
+    loading:{
+        width: RFValue(40),
+        height: RFValue(40)
+    },
+    con:{
+        flex:1,
+        backgroundColor: "#fff"
+    },
+    logo:{
+        paddingRight: RFValue(100),
+        width:RFValue(20),
+        height:RFValue(20)
+    },
+    appName:{
+        fontWeight: "bold",
+        fontSize: 25
+    },
+    weatherCon:{
+        flex:0.5,
+        marginBottom: 5,
+        borderWidth: 1.6,
+        borderColor: "#20bed4"
+    },
+    weatherText: {
+        textAlign: "center",
+    },
+    whatToTake:{
+        margin: 20,
+        borderWidth: 1.6,
+        borderRadius: 40,
+        borderColor: "#20bed4"
+    }
+})
